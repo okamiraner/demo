58646336c19f1f6557d043d7de80570afd94fd79
okamiraner <c.mararenko@gmail.com>
2025-06-07 23:31:20 +0300

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,24 @@
-import nextra from 'nextra'
+import 'dotenv/config';
+import nextra from 'nextra';
 
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-}
+  basePath: isProd ? '/demo' : '',
+  assetPrefix: isProd ? '/demo/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 const withNextra = nextra({
 
-})
+});
 
 
 export default withNextra({
   ...nextConfig,
   contentDirBasePath: '/content',
-})
+});
