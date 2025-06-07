478d71d7326be2b80e0029c9ca15145bc440ccfe
okamiraner <c.mararenko@gmail.com>
2025-06-07 21:37:57 +0300

    Fix

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from 'next';
+import nextra from 'nextra';
+
+
+const nextConfig: NextConfig = {
+  output: 'export',
+};
+
+const withNextra = nextra({
+  contentDirBasePath: '/content',
+});
+
+
+export default withNextra({
+  ...nextConfig,
+  ...withNextra,
+});
